87f9ccac1c1e3466b4aace980b7b5525e6234538
Peter Tonner <peter.tonner@gmail.com>
2017-02-16 12:54:46 -0500

    start some nn computation

--- shipnn.py
+++ shipnn.py
@@ -14,7 +14,7 @@ class ShipNN(Ship):
         # self.acceleration = [0,0]
 
         # self.net = nl.net.newff(weights)
-        self.net = nl.net.newff([[-10,10] for i in range(5)], [5,5, 4])
+        self.net = nl.net.newff([[-10,10] for i in range(2)], [5,5, 4])
 
         for l in self.net.layers:
             l.initf = nl.init.InitRand([-1., 1.], 'wb')
@@ -24,19 +24,9 @@ class ShipNN(Ship):
 
     def buildInput(self):
 
-        # print self.angle, self.velocity
+        y,x = self.positions()
 
-        playerVec = np.array([self.world.player.position[0]-self.position[0], self.world.player.position[1]-self.position[1]])
-        playerVec = playerVec/np.linalg.norm(playerVec)
-        playerDot = np.dot(np.array(self.velocity)/np.linalg.norm(self.velocity), playerVec)
-
-        distVelocity = np.add(self.velocity, self.world.player.velocity)
-        distVelocityNorm = np.linalg.norm(distVelocity)
-
-        # return [[self.angle, self.velocity[0], self.velocity[1], playerDot, random.random()*10-5]]
-        # return [[self.velocity[0], self.velocity[1], playerDot, distVelocityNorm]]
-        # return [[self.velocity[0], self.velocity[1]] + distVelocity.tolist()]
-        return [self.velocity + distVelocity.tolist() + [playerDot]]
+        return [y+x]
 
     def compute(self,v):
 
@@ -72,27 +62,20 @@ class ShipNN(Ship):
         # print self.positions()
 
 
-        # ivec = self.buildInput()
-        # ovec = self.compute(ivec)[0]
+        ivec = self.buildInput()
+        print np.array(ivec).shape, self.net.ci
+        ovec = self.compute(ivec)[0]
+
+        print ovec
 
         # print self.angle-ovec[0], self.velocity[0] - ovec[1], self.velocity[1] - ovec[2]
 
-        # self.angle = ovec[0]
-        # self.velocity[0] = ovec[1]
-        # self.velocity[1] = ovec[2]
+        # self.rotate_by(int(ovec[0]*6))
+        self.rotate_by(ovec[0])
 
-        # self.velocity[0] += ovec[1]
-        # self.velocity[1] += ovec[2]
+        self.thrust(ovec[1]>0)
 
-        # self.velocity[0] *= (1+ovec[1])
-        # self.velocity[1] *= (1+ovec[2])
-
-        # mag = np.sqrt((np.array(self.velocity)**2).sum()) * (3**ovec[3])
-        # self.velocity = [self.velocity[0]/mag, self.velocity[1]/mag]
-
-        # if self.world.alien_time == 0:
-        #     print ivec, ovec, self.velocity
-
-        # print self.angle, self.velocity
-
+        if ovec[2] > 0:
+            self.fire()
+            
         super(ShipNN, self).update()
